8ce8ccefde349b43278242e810197cb260e78693
Rafael Rodriguez <rrg1459@hotmail.com>
2017-09-05 19:09:38 -0400

    2do commit

--- app/models/mensaje.rb
+++ app/models/mensaje.rb
@@ -18,19 +18,20 @@ private
       cg = ContactosGrupo.where(grupo_id: grp.id) 
       cg.each do |cgl|
         contacto = Contacto.find(cgl.contacto_id)
-        puts 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-        puts "Id mensaje: #{self.id}, ID contacto: #{contacto.id}"
+        puts '..........................................'
+        puts "ID contacto: #{contacto.id}, ID grupo: #{grp.id}, ID mensaje: #{self.id}"
         puts "Nombre grupo: #{grp.nombre}, contacto: #{contacto.nombre}"
-        puts 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-         vector[contador] = contacto.id
-         contador += 1
+        puts '..........................................'
+        vector[contador] = contacto.id
+        contador += 1
       end
     end
     vector = vector.uniq
-    cant = vector.length
-    puts cant
-    puts vector.inspect
+    cantidad = vector.length
+    puts "ID del mensaje.....: #{self.id}"
+    puts "Total de contactos.: #{cantidad}"
+    puts "ID de los contactos: #{vector.inspect}"
     mensaje = Mensaje.find(self.id)
-    mensaje.update_columns(cantidad_contactos: cant)
+    mensaje.update_columns(cantidad_contactos: cantidad)
   end
 
